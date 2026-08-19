a32f71874da3baf33c289c3532bc9143d39a3794
purvanshsingh784-dot <purvanshsingh784@gmail.com>
2026-08-19 14:41:24 +0530

    hello

---
+++ day4/node.js
@@ -0,0 +1,127 @@
+let name = "Rahul";
+const age = 20;
+var city = "Delhi";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+let username = "Alex";
+
+function showName() {
+    console.log(username);
+}
+
+showName();
+
+let firstName = "Purvansh";
+let lastName = "Singh";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+//split()
+let data ='CSS,INDEX,JAVASCRIPT';
+let result=data.split(',');
+console.log(result);
+
+
+// include()
+
+
+let course="javascript";
+console.log(
+    course.includes("python")
+);
+console.log(
+    course.includes("javascript")
+);
+
+// ARRAY OBJECT
+
+console.log("===ARRAY OBJECTS===")
+
+// ACCESS ARRAY ELEMENTS
+
+console .log(numbers[0])
+console.log(numbers[1])
+
+//ARRAY METHODS
+
+console.log("===ARRAY METHODS===")
+//push
+
+
+// unshift()
+numbers.unshift()
+
+// shift()
+
+numbers.shift()
+
+
+// dateobject   
+let date=new date();
+console.log(data);
+
+// get full year
+
+console.log(
+    "year=",
+    date.getmonth()
+);
+
+// get date
+
+console.log(
+    "Date=",
+    date.getDate()
+);
+// maths object
+
+
+
+// mAXIMUM VALLUE
+
+console.log(
+    "Maximum=",
+    Math.max[10,20,30,40]
+);
+
+console.log(
+    "Minimum=",
+    Math.min[10,20,30,40]
+);
+
+//round
+
+console.log(
+    "Round=",
+    Math.round[45.7]
+);
+
+//floor
+console.log(
+    "Floor=",
+    Math.floor(5.9)
+);
+//square root
+console.log(
+    "Square root=",
+    Math.square(4)
+);
+//cell
+console.log(
+    "cell=",
+    Math.cell()
+);
+// power
+console.log(
+    "Power=",
+    Math.power(4)
+);
+//
---
+++ percentage.js
@@ -0,0 +1,63 @@
+﻿console.log("==== Variables ====") ;
+
+var text = "something" ;
+console.log(text) ;
+
+console.log("===== If-Else =====") ;
+
+var a = 20 ;
+var b = 10 ;
+
+if(a>b){
+    console.log("A is greater than B") ;
+}
+else{
+    console.log("B is greater than A. ") ;
+}
+
+
+console.log("===== function with parameters==== ");
+function greet(fname , lname){
+    console.log("Hello" , fname , lname) ;
+}
+greet("Utkarsh"," Dixit") ;
+greet("Waseem ","Ahmed") ;
+
+
+function add(x,y){
+    return x+y ;
+}
+
+var result = add(10,20) ;
+console.log("sum = " , result) ;
+
+
+console.log("======Default Argument =====") ;
+
+function fullName(fname = "ABES" , lname = "Engineering College") {
+    return fname + " " + lname ; 
+}
+
+console.log(fullName()) ;
+console.log(fullName("Utkarsh","Dixit")) ;
+
+
+console.log("=== Percentage Function ===")
+
+function percentage(total){
+    return total/3 ;
+}
+function totalMarks(math , english , science){
+    return math+english+science ;
+}
+
+var total = totalMarks(80,75,90) ;
+console.log("TotalMarks : " , total );
+console.log("percentage : " , percentage(total) + "%") ;
+
+
+// Global Variable 
+
+console.log("===== Global Variable ====");
+
+console.log("String Pitch---> ") ;
